eca13f4500ed0153fdf2aca7ed3da80c90875a34
vhenri <victoriabhenri@gmail.com>
2022-10-15 14:31:46 -0400

    Adding retrofit, moshi & kotlin-result dependencies. Added Stock model

---
+++ app/src/main/java/com/vhenri/stock_list_app/models/Stock.kt
@@ -0,0 +1,19 @@
+package com.vhenri.stock_list_app.models
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Stock(
+    val ticker: String,
+    val name: String,
+    val currency: String,
+    @Json(name = "current_price_cents") val currentPriceCents: Int,
+    val quantity: Int?,
+    @Json(name = "current_price_timestamp") val timestamp: Int // TODO - make Instant?
+)
+
+// TODO - use enums for currency!
+enum class Currency {
+    USD,
+}
