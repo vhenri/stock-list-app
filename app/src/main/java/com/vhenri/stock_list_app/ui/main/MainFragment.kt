0d3522e679a13f2c1c4775a8f1ebf047126102f9
vhenri <victoriabhenri@gmail.com>
2022-10-15 13:54:37 -0400

    initial android fragment+vm generated by AS

---
+++ app/src/main/java/com/vhenri/stock_list_app/ui/main/MainFragment.kt
@@ -0,0 +1,32 @@
+package com.vhenri.stock_list_app.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.vhenri.stock_list_app.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/vhenri/stock_list_app/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.vhenri.stock_list_app.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
